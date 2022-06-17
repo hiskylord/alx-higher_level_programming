98131f0fcf6fd65a0f6e2aefa868afbdcd5c04aa
Erubure David Ejiro <davidejiro49a@gmail.com>
2022-06-17 05:24:09 +0100

    ready

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,43 @@
 #include "lists.h"
 
 /**
- * check_cycle - list
- * @head:  type list
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer
+ * Return: 0 if there is no cycle,
+ * and 1 if there is a cycle
  */
-int check_cycle(listint_t *head)
+int check_cycle(listint_t *list)
 {
-	int *node1, *node2;
+	listint_t *p2;
+	listint_t *previous;
 
-	if (head == NULL)
-		return (0);
-
-	while (head != NULL)
+	p2 = list;
+	previous = list;
+	while (list && p2 && p2->next)
 	{
-		node1 = (int *)&head;
-		node2 = (int *)&head->next;
-		if (head->next == NULL)
-			return (0);
+		list = list->next;
+		p2 = p2->next->next;
 
-		if (*node1 - *node2 <= 0)
+		if (list == p2)
+		{
+			list = previous;
+			previous = p2;
+			while (1)
+			{
+				p2 = previous;
+				while (p2->next != list && p2->next != previous)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
-
-		head = head->next;
+		}
 	}
+
 	return (0);
 }
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+from sys import argv
+
+if (__name__ == "__main__"):
+    i = 1
+    result = 0
+    arguments = len(argv) - 1
+
+    if (arguments != 0):
+        while (i <= arguments):
+            result += int(argv[i])
+            i += 1
+    print(result)
