516c713fd54a34bb6d157d1a8c7cf43e7377e8cb
Erubure David Ejiro <davidejiro49a@gmail.com>
2022-08-26 19:38:03 +0100

    udb

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,5 @@
-
--- Convert 'hbtn_0c_0' to utf8mb4, collate utf8mb4_unicode_ci
--- Convert db 'hbtn_0c_0'
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
--- Convert table 'first_table'
+-- Script that converts hbtn_0c_0 database to UTF8
+-- Query to convert hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert field 'name' in 'first_table'
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
